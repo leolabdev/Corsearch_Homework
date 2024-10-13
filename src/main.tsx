ff354ed59d8b486f3eadd7b20c93a0931c831c02
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-13 19:47:55 +0300

    Refactor import paths to use absolute paths
    This change updates the import statements in `src/main.tsx` to use absolute paths instead of relative paths. Absolute paths improve code readability and maintainability.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app/App.tsx';
-import './app/styles/index.scss';
-import {Providers} from './app/providers';
+import App from '@/app/App.tsx';
+import '@/app/styles/index.scss';
+import {Providers} from '@/app/providers';
 
 
 createRoot(document.getElementById('root')!).render(
