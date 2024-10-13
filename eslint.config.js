3f7d00766c80b61a35660c890bc44dcfffc6920c
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-10-13 17:57:03 +0300

    Add @tanstack/eslint-plugin-query to development setup
    Included @tanstack/eslint-plugin-query in devDependencies for enhanced linting capabilities related to Tanstack Query. Adjusted ESLint configuration to extend recommended rules and added relevant import in eslint.config.js. Corrected a minor typo in the import path in src/main.tsx.

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,25 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import query from '@tanstack/eslint-plugin-query';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+        js.configs.recommended,
+        ...tseslint.configs.recommended,
+        'plugin:@tanstack/eslint-plugin-query/recommended'
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+        '@tanstack/query': query,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
